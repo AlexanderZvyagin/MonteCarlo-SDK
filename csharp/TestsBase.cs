7c4b8d1adab0923226b163c302eb9c3e54fa375a
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2022-05-10 23:20:46 +0100

    Change the default port.

--- csharp/TestsBase.cs
+++ csharp/TestsBase.cs
@@ -3,6 +3,6 @@ using NUnit.Framework;
 [TestFixture]
 public class TestsBase
 {
-    public static readonly string server = "http://naz.hopto.org:8080";
+    public static readonly string server = "http://naz.hopto.org:4401";
     public static readonly double abseps = 1e-7;
 }
