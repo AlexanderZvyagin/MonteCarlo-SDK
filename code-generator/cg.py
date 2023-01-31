b6a3354e7b4dda004a9f0af959aee22c29e4563e
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-01-31 20:19:46 +0000

    Call base ctor.

--- code-generator/cg.py
+++ code-generator/cg.py
@@ -115,7 +115,9 @@ def Struct_python (self:Struct):
 def Function_cpp(self:Function, obj:Struct=None):
     ftype = cpp_types[self.type]+' '
     ctor = False
+    derived = False
     if obj:
+        derived = obj.base is not None
         if self.name==obj.name:
             ftype = '' # constructor
             ctor = True
@@ -123,15 +125,34 @@ def Function_cpp(self:Function, obj:Struct=None):
 
     code = []
     code.append(f'{ftype}{self.name}(')
+
+    attributes = []
+    if ctor and obj.base:
+        attributes = obj.base.attributes + obj.attributes
+    elif ctor:
+        attributes = obj.attributes
+    else:
+        attributes = self.args
+
     args_code = []
-    for a in (obj.attributes if ctor else self.args):
+    for a in attributes:
         args_code.append(f'{indent}{cpp_types[a.type]} {a.name}')
     for i in range(len(args_code)-1):
         args_code[i] += ','
     code.extend(args_code)
 
 
-    code.append('){')
+    if derived:
+        code.append(f') : {obj.base.name} (')
+        args_code = []
+        for a in obj.base.attributes:
+            args_code.append(f'{indent}{a.name}')
+        for i in range(len(args_code)-1):
+            args_code[i] += ','
+        code.extend(args_code)
+        code.append('){')
+    else:
+        code.append('){')
     if ctor:
         for a in obj.attributes:
             code.append(f'{indent}this->{a.name} = {a.name};')
@@ -166,7 +187,9 @@ def Struct_cpp (self:Struct):
 def Function_typescript (self:Function, obj:Struct=None):
     fname = self.name
     ctor = False
+    derived = False
     if obj:
+        derived = obj.base is not None
         if self.name==obj.name:
             fname = 'constructor'
             ctor = True
@@ -174,8 +197,18 @@ def Function_typescript (self:Function, obj:Struct=None):
 
     code = []
     code.append(f'{fname}(')
+
+
+    attributes = []
+    if ctor and obj.base:
+        attributes = obj.base.attributes + obj.attributes
+    elif ctor:
+        attributes = obj.attributes
+    else:
+        attributes = self.args
+
     args_code = []
-    for a in (obj.attributes if ctor else self.args):
+    for a in attributes:
         args_code.append(f'{indent}{a.name} : {typescript_types[a.type]}')
     for i in range(len(args_code)-1):
         args_code[i] += ','
@@ -183,11 +216,31 @@ def Function_typescript (self:Function, obj:Struct=None):
 
     code.append('){')
 
-    if ctor:
-        for a in obj.attributes:
-            code.append(f'{indent}this.{a.name} = {a.name};')
-    else:
-        print('no code')
+    if derived:
+        code.append(f'{indent}super (')
+        args_code = []
+        for a in obj.base.attributes:
+            args_code.append(f'{indent*2}{a.name}')
+        for i in range(len(args_code)-1):
+            args_code[i] += ','
+        code.extend(args_code)
+        code.append(f'{indent})')
+
+
+    # args_code = []
+    # for a in (obj.attributes if ctor else self.args):
+    #     args_code.append(f'{indent}{a.name} : {typescript_types[a.type]}')
+    # for i in range(len(args_code)-1):
+    #     args_code[i] += ','
+    # code.extend(args_code)
+
+    # code.append('){')
+
+    # if ctor:
+    #     for a in obj.attributes:
+    #         code.append(f'{indent}this.{a.name} = {a.name};')
+    # else:
+    #     print('no code')
 
     code.append('}')
 
@@ -211,8 +264,40 @@ def Struct_typescript (self:Struct):
             code.append(f'{indent}{line}')
         code.append('')
 
-    code.append('')
     code.append(f'}}')
+    code.append('')
+    return code
+
+autogen_text = 'This is an automatically generated file.'
+
+def File_prefix_cpp (objs):
+    return [
+        f'// {autogen_text}',
+        '',
+        '#include <string>',
+        '#include <vector>',
+        ''
+    ]
+
+def File_prefix_python (objs):
+    return [
+        f'# {autogen_text}',
+        ''
+    ]
+
+def File_prefix_typescript (objs):
+    return [
+        f'// {autogen_text}',
+        ''
+    ]
+
+def File_suffix_typescript (objs):
+    code = []
+    code.append('export {')
+    for i,obj in enumerate(objs):
+        c = ',' if (i+1)<len(objs) else ''
+        code.append(f'{indent}{obj.name}' + c)
+    code.append('}')
     return code
 
 def test_main ():
@@ -293,29 +378,62 @@ def test_files ():
     UpdaterDto = obj
 
     obj = Struct('Updater',UpdaterDto)
-    # obj.attributes.append(Variable('name','string',None))
-    # obj.attributes.append(Variable('refs','int[]',None))
-    # obj.attributes.append(Variable('args','float[]',None))
-    # obj.attributes.append(Variable('start','float',None))
     obj.attributes.append(Variable('_equation','int',-1))
     obj.attributes.append(Variable('_state','int',-1))
     obj.methods.append(Function (obj.name,''))
     objs.append(obj)
 
     for language in ('python','cpp','typescript'):
-        with open(fname+'.'+ext[language],'w') as file:
-            for obj in objs:
-                if isinstance(obj,Struct):
-                    name = f'Struct_{language}'
-                    func = globals().get(name)
-                    if not func:
-                        print(f'Not found: "{name}"')
-                        continue
+        write_objs(fname,language,objs)
 
-                    for line in func(obj):
-                        file.write(line+'\n')
-                else:
-                    print(f'Cannot handle {type(obj)}')
+    # for language in ('python','cpp','typescript'):
+    #     with open(fname+'.'+ext[language],'w') as file:
+    #         file_prefix_code = globals().get(f'File_prefix_{language}')
+    #         if file_prefix_code:
+    #             for line in file_prefix_code(objs):
+    #                 file.write(line+'\n')
+    #         for obj in objs:
+    #             if isinstance(obj,Struct):
+    #                 name = f'Struct_{language}'
+    #                 func = globals().get(name)
+    #                 if not func:
+    #                     print(f'Not found: "{name}"')
+    #                     continue
+
+    #                 for line in func(obj):
+    #                     file.write(line+'\n')
+    #             else:
+    #                 print(f'Cannot handle {type(obj)}')
+
+    #         file_suffix_code = globals().get(f'File_suffix_{language}')
+    #         if file_suffix_code:
+    #             for line in file_suffix_code(objs):
+    #                 file.write(line+'\n')
+
+def write_objs(fname:str,language,objs=[]):
+    '''fname: full path name without extension, the file will be overwritten. the directory path must exist.'''
+    with open(fname+'.'+ext[language],'w') as file:
+        file_prefix_code = globals().get(f'File_prefix_{language}')
+        if file_prefix_code:
+            for line in file_prefix_code(objs):
+                file.write(line+'\n')
+        for obj in objs:
+            if isinstance(obj,Struct):
+                name = f'Struct_{language}'
+                func = globals().get(name)
+                if not func:
+                    print(f'Not found: "{name}"')
+                    continue
+
+                for line in func(obj):
+                    file.write(line+'\n')
+            else:
+                print(f'Cannot handle {type(obj)}')
+
+        file_suffix_code = globals().get(f'File_suffix_{language}')
+        if file_suffix_code:
+            for line in file_suffix_code(objs):
+                file.write(line+'\n')
 
 if __name__ == '__main__':
     test_files()
