61db1d23fe05056d14e1dd5a783e0ccaf1279bdf
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-03-17 08:40:08 +0000

    Add helper function to run models.

--- python/notebooks/init.py
+++ python/notebooks/init.py
@@ -3,6 +3,12 @@ logging.basicConfig (
     format = '%(asctime)s [%(levelname)s] %(message)s',
     level = logging.INFO
 )
-from mcsdk import *
-#server = 'http://localhost:8080'
+import mcsdk as sdk
 server = 'http://naz.hopto.org:8001'
+
+def run(model:sdk.Model) -> sdk.EvaluationResults:
+    response = requests.post(f'{server}/model',sdk.Model_to_json_string(model))
+    assert response.status_code == 200
+    er = sdk.EvaluationResults_from_json_string(response.text)
+    er.model = model
+    return er
