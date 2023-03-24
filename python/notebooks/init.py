2ec8bf0523e7608f1652d456861f3069ec4d6a05
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-03-24 14:18:46 +0000

    Handle errors.

--- python/notebooks/init.py
+++ python/notebooks/init.py
@@ -6,9 +6,17 @@ logging.basicConfig (
 import mcsdk as sdk
 server = 'http://naz.hopto.org:8001'
 
+class McSdkException(Exception):
+    def __init__ (self,error:sdk.Error):
+        super().__init__(error.message)
+        self.error = error
+    
 def run(model:sdk.Model) -> sdk.EvaluationResults:
     response = requests.post(f'{server}/model',sdk.Model_to_json_string(model))
-    assert response.status_code == 200
-    er = sdk.EvaluationResults_from_json_string(response.text)
-    er.model = model
-    return er
+    if response.status_code == 200:
+        result = sdk.EvaluationResults_from_json_string(response.text)
+        result.model = model
+        return result
+    else:
+        error = sdk.Error_from_json_string(response.text)
+        raise McSdkException (error)
