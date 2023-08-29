0ab65a355d9fc27d88efe051203ac33a1811a52f
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-08-29 23:43:54 +0100

    Use sdk.run(model,server). Fix t07_HullWhiteModel notebook.

--- python/notebooks/init.py
+++ python/notebooks/init.py
@@ -5,18 +5,3 @@ logging.basicConfig (
 )
 import mcsdk as sdk
 server = 'http://naz.hopto.org:8001'
-
-class McSdkException(Exception):
-    def __init__ (self,error:sdk.Error):
-        super().__init__(error.message)
-        self.error = error
-    
-def run(model:sdk.Model) -> sdk.EvaluationResults:
-    response = requests.post(f'{server}/model',sdk.Model_to_json_string(model))
-    if response.status_code == 200:
-        result = sdk.EvaluationResults_from_json_string(response.text)
-        result.model = model
-        return result
-    else:
-        error = sdk.Error_from_json_string(response.text)
-        raise McSdkException (error)
