723893e287f848095d74151bc0acfa7f895aa24e
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-11-15 22:10:30 +0000

    - Use matplotlib for plotting - update/improve histograms  notebook

--- python/notebooks/init.py
+++ python/notebooks/init.py
@@ -4,5 +4,6 @@ logging.basicConfig (
     level = logging.INFO
 )
 import mcsdk as sdk
+from plot import plot
 import os
 server = f'http://{os.getenv("SERVER_ADDRESS","naz.hopto.org")}:{os.getenv("SERVER_PORT","8001")}'
---
+++ python/notebooks/plot.py
@@ -0,0 +1,43 @@
+import mcsdk
+
+import matplotlib.pyplot as plt
+
+def linspace(min:float,max:float,n:int):
+    assert n>=1 and min!=max
+    step = (max-min)/n
+    return [min] + [min+step*i for i in range(1,n)] + [max]
+
+def plot(results:mcsdk.EvaluationResults,point:int,stateX:int,stateY:int|None=None,histogram_index=0):
+    fig, ax = plt.subplots(tight_layout=True)
+    ax.set_title(f'Extraction point {point} out of {len(results.time_points)}, time={results.time_points[point]}')
+    histograms_filtered = [h for h in results.histograms if h.evaluation_point==point and h.ax.state==stateX]
+    if stateY is None:
+        histograms_filtered = [h for h in histograms_filtered if not h.ay]
+        if len(histograms_filtered)<=histogram_index:
+            raise Exception('Cannot find the requested 1D histogram.')
+        h = histograms_filtered[histogram_index]
+        
+        edges = linspace(h.ax.min,h.ax.max,h.ax.nbins)
+        ax.hist(edges[:-1], edges, weights=h.bins)
+    else:
+        # 2D histogram
+        histograms_filtered = [h for h in histograms_filtered if h.ay and h.ay.state==stateY]
+        if len(histograms_filtered)<=histogram_index:
+            raise Exception('Cannot find the requested 2D histogram.')
+        h = histograms_filtered[histogram_index]
+
+        x_edges = linspace(h.ax.min,h.ax.max,h.ax.nbins)
+        y_edges = linspace(h.ay.min,h.ay.max,h.ay.nbins)
+
+        vx = []
+        vy = []
+        for y in y_edges[:-1]:
+            for x in x_edges[:-1]:
+                vx.append(x)
+                vy.append(y)
+
+        plt.hist2d(vx,vy,bins=[x_edges,y_edges],weights=h.bins)
+        ax.set_ylabel(results.model.titles[stateY])
+
+    plt.xlabel(results.model.titles[stateX])
+    plt.show()
