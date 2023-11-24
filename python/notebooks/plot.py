8dd974674201e7b222c66a37f670818210ba9ae2
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2023-11-24 14:47:34 +0000

    Add protection against badly strcutred histograms.

--- python/notebooks/plot.py
+++ python/notebooks/plot.py
@@ -3,13 +3,14 @@ import mcsdk
 import matplotlib.pyplot as plt
 
 def linspace(min:float,max:float,n:int):
-    assert n>=1 and min!=max
+    if not (n>=1 and min!=max):
+        raise Exception(f'Bad histogram axis: nbins={n} range=[{min},{max}]')
     step = (max-min)/n
     return [min] + [min+step*i for i in range(1,n)] + [max]
 
 def plot(results:mcsdk.EvaluationResults,point:int,stateX:int,stateY:int|None=None,histogram_index=0):
     fig, ax = plt.subplots(tight_layout=True)
-    ax.set_title(f'Extraction point {point} out of {len(results.time_points)}, time={results.time_points[point]}')
+    ax.set_title(f'Extraction point {point} out of [0,{len(results.time_points)}), time={results.time_points[point]}')
     histograms_filtered = [h for h in results.histograms if h.evaluation_point==point and h.ax.state==stateX]
     if stateY is None:
         histograms_filtered = [h for h in histograms_filtered if not h.ay]
