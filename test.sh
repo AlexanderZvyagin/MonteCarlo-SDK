9b004e338ff0fd97ca5e4be3d0a949bb6c6cb4af
zvyagin <zvyagin.alexander@gmail.com>
2024-10-27 17:28:18 +0000

    minor improvements

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #/usr/bin/env bash
-export SERVER_ADDRESS=az.hopto.org
-export SERVER_PORT=8000
-(cd typescript && ./test.sh)
+. set_default_server.sh
+(cd typescript && npm install && ./test.sh)
 (cd python/test && ./test.sh)
