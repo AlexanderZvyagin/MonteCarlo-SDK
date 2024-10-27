97bc25ba0c8cd29148b95477fd941446ff283a22
zvyagin <zvyagin.alexander@gmail.com>
2024-10-27 16:48:23 +0000

    Code refactoring.

--- typescript/server-access.test.ts
+++ typescript/server-access.test.ts
@@ -4,23 +4,20 @@ import {server} from './server.js'
 import * as sdk from './mcsdk'
 
 test('modules are loaded', () => {
-  expect(fetch).not.toBeUndefined()
-  expect(server()).not.toBeUndefined()
-  expect(sdk).not.toBeUndefined()
-  expectTypeOf(sdk.Model).not.toBeUndefined()
+    expect(fetch).not.toBeUndefined()
+    expect(server()).not.toBeUndefined()
+    expect(sdk).not.toBeUndefined()
+    expectTypeOf(sdk.Model).not.toBeUndefined()
 })
 
 test('server endpoints are accessible', async function () {
-        for(let endpoint of ['/','/metrics','/functions']){
-            await fetch(`${server()}${endpoint}`,{
-                // method: 'post',
-                // body:JSON.stringify(null),
-                // headers: { 'Content-Type': 'application/json'}
-            })
-            .then(result => {
-                expect(result.status).toBe(200);
-                expect(result.error).toBe(undefined);
-            })
-//            .catch(error=>throw new Error(error));
-        }
+    console.log(`The server is ${server()}`);
+    for(let endpoint of ['/','/metrics','/functions']){
+        await fetch(`${server()}${endpoint}`,{
+        })
+        .then(result => {
+            expect(result.status).toBe(200);
+            expect(result.error).toBe(undefined);
+        })
+    }
 })
