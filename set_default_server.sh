7d2f08841a581d0a92ca90c99c20342c381a7e48
zvyagin <zvyagin.alexander@gmail.com>
2025-03-03 23:20:45 +0000

    Update

--- set_default_server.sh
+++ set_default_server.sh
@@ -1,2 +1,3 @@
 #/usr/bin/env bash
-export SERVER_ADDRESS=https://mc0.netlify.app/engine-8200
+#export SERVER_ADDRESS=https://mc0.netlify.app/engine-8200
+export SERVER_ADDRESS=http://localhost:8201
